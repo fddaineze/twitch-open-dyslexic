3baae5297a4964772d81867bed5960e55a550ddd
Filipe Daineze <filipe.daineze@outlook.com>
2024-02-16 16:08:00 -0300

    Add youtube support

--- src/script.js
+++ src/script.js
@@ -1,50 +1,80 @@
 console.log("%c---> Twitch Dyslexic has started", "color:green");
-const interval = setInterval(startBuild, 1000);
+
+const interval = setInterval(startBuild, 2000);
 
 let fontSize = 13;
 
 function startBuild() {
-    const streamChatRoom = document.querySelector("div.stream-chat .chat-room");
-    const streamChat = document.querySelector("div.stream-chat");
+    const currentUrl = window.location.href;
+    let streamChatRoom, streamChat;
+    if (currentUrl.includes("twitch.tv")) {
+        console.log("include twitch")
+        streamChatRoom = document.querySelector("div.stream-chat .chat-room");
+        streamChat = document.querySelector("div.stream-chat");
+    } else if (currentUrl.includes("youtube.com")) {
+        console.log("include youtube");
+        streamChatRoom = document.getElementById("chatframe");
+        streamChat = document.getElementById("chat-container");
 
-    const fs = parseInt(localStorage.getItem('dyslexic-font-size'));
-    if(fs) {
-        fontSize = fs;
-        setFontSize(streamChatRoom, fontSize);
+        if (streamChatRoom.tagName.toLowerCase() === 'iframe') {
+            // Obtenha a referência para o documento dentro do iframe
+            const iframeDocument = streamChatRoom.contentDocument || streamChatRoom.contentWindow.document;
+            streamChatRoom = iframeDocument.getElementById("chat");
+
+            const style = iframeDocument.createElement('style');
+            style.textContent = `
+                @font-face {
+                    font-family: OpenDyslexic;
+                    src: url(https://fddaineze.github.io/demo/font/OpenDyslexic-Regular.woff2);
+                }
+                yt-live-chat-text-message-renderer {
+                    font-size: unset !important;
+                }
+            `;
+            iframeDocument.head.appendChild(style);
+        }
     }
-    
-    const ff = localStorage.getItem('dyslexic-font-family');
-    if(ff) switchFontFamily(streamChatRoom);
+    console.log("streamChatRoom",streamChatRoom)
+    console.log("streamChat",streamChat)
 
-    const dyslexicHeader = document.createElement('div');
-    dyslexicHeader.classList.add('dyslexic-header');
-    streamChat.prepend(dyslexicHeader);
-    
-    if (streamChat) {
+    if (streamChatRoom && streamChat) {
+        const fs = parseInt(localStorage.getItem('dyslexic-font-size'));
+        if(fs) {
+            fontSize = fs;
+            setFontSize(streamChatRoom, fontSize);
+        }
+        
+        const ff = localStorage.getItem('dyslexic-font-family');
+        if(ff) switchFontFamily(streamChatRoom);
+
+        const dyslexicHeader = document.createElement('div');
+        dyslexicHeader.classList.add('dyslexic-header');
+        streamChat.prepend(dyslexicHeader);
+        
         clearInterval(interval);
         createButton(dyslexicHeader, "Switch Font", "switchFontButton");
         createButton(dyslexicHeader, "A+", "fontPlusButton");
         createButton(dyslexicHeader, "A-", "fontMinusButton");
         createButton(dyslexicHeader, "Reset", "fontResetButton");
+        
+        document.getElementById("switchFontButton").addEventListener("click", () => {
+            switchFontFamily(streamChatRoom);
+        });
+        
+        document.getElementById("fontPlusButton").addEventListener("click", () => {
+            fontSize += 2;
+            setFontSize(streamChatRoom, fontSize);
+        });
+        
+        document.getElementById("fontMinusButton").addEventListener("click", () => {
+            fontSize -= 2;
+            setFontSize(streamChatRoom, fontSize);
+        });
+        
+        document.getElementById("fontResetButton").addEventListener("click", () => {
+            resetFont(streamChatRoom);
+        });
     }
-
-    document.getElementById("switchFontButton").addEventListener("click", () => {
-        switchFontFamily(streamChatRoom);
-    });
-    
-    document.getElementById("fontPlusButton").addEventListener("click", () => {
-        fontSize += 2;
-        setFontSize(streamChatRoom, fontSize);
-    });
-    
-    document.getElementById("fontMinusButton").addEventListener("click", () => {
-        fontSize -= 2;
-        setFontSize(streamChatRoom, fontSize);
-    });
-    
-    document.getElementById("fontResetButton").addEventListener("click", () => {
-        resetFont(streamChatRoom);
-    });
 }
 
 function createButton(header, name, cname) {
